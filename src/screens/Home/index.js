aea310406579c13853836b18454ce8a2ae534dbe
jszvitor <jszvitor@gmail.com>
2020-06-26 00:47:14 -0300

    feat: added Home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,2 +1,24 @@
+import React from 'react';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+
+import { Container, Wrapper, Header, BalenceContainer, BalanceTitle, Balance } from './styles';
+
 import Suggestions from '../../components/Suggestions';
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10cb62" />
+          <BalenceContainer>
+            <BalanceTitle>Meu saldo</BalanceTitle>
+            <Balance>R$ 0,80</Balance>
+          </BalenceContainer>
+          <AntDesign name="gift" size={30} color="#10cb62" />
+        </Header>
         <Suggestions />
+      </Container>
+    </Wrapper>
+  )
+}
